1addf086485c8727eb4f57a53dc40cc6926b1beb
Helmy Krisdin Garcia <helmygarcia67@gmail.com>
2025-07-31 15:56:44 +0700

    login & register card

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Cardwp from "./Cardwp"
+
+export default function LoginCard() {
+  return (
+    <div className="items-center justify-items-center min-h-screen sm:p-50">
+        <Cardwp />
+    </div>
+  )
+}
---
+++ frontend/src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import Regcard from "./RegCard";
+
+export default function RegisterCard() {
+  return (
+    <div className="items-center justify-items-center min-h-screen sm:p-50">
+      <Regcard />
+    </div>
+  );
+}
